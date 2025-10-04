81cf66fdc510b1f71de2d97fac79a636c19718bd
Avez Chadchan <avezchadchan@gmail.com>
2025-10-05 00:55:37 +0530

    mongoDB connected

--- production_backend/src/server.js
+++ production_backend/src/server.js
@@ -1,12 +1,39 @@
-import express from 'express'
+//1 do DB related oprations(creation) on another file and import it here
+//first import dotenv in the main file
+import dotenv from "dotenv";
+import connectDB from "./db/index.js"
 
-const app=express();
+dotenv.config();
+connectDB();
 
-app.get('/',(req,res)=>{
-    res.send("SERver is READY")
-});
-const port =3000;
-app.listen(port,()=>{
-    console.log("http://localhost:3000");
-    
-})
+//2 do DB related operations on same file
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+
+// import express from "express";
+// const app = express();
+
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// /*write semicolon before iife if the previous life doesn't have 1 then it will be useful; */
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//     console.log(" MongoDB connected");
+//     app.on("error", (error) => {
+//       console.log("ERROR: ", error);
+//       throw error;
+//     });
+//     app.get('/',(req,res)=>{
+//         res.send('MongoDB is connected successfully')
+//     })
+//     app.listen(process.env.PORT || 3000, () => {
+//       console.log(`App is listening on http://localhost:${process.env.PORT || 3000}`);
+//     });
+//   } catch (error) {
+//     console.log("Error:", error);
+//     throw error;
+//   }
+// })();
