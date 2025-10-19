00460d411304bf41f387bab3cfc005cac1838d2d
Avez Chadchan <avezchadchan@gmail.com>
2025-10-19 23:16:33 +0530

    asyncHandler,apierror

--- production_backend/src/server.js
+++ production_backend/src/server.js
@@ -1,17 +1,47 @@
 //1 do DB related oprations(creation) on another file and import it here
 //first import dotenv in the main file
 import dotenv from "dotenv";
-import connectDB from "./db/index.js"
-
+import connectDB from "./db/index.js";
+import express from "express";
+import { asyncHandler } from "./utils/asyncHandler.js";
+const app = express();
 dotenv.config();
-connectDB();
+connectDB()
+  .then((req, res) => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.get(
+      "/",
+      asyncHandler(async (req, res) => {
+        res.send("MongoDB connected!!!");
+      })
+    );
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongo db connection failed!!!!!!!!!", err);
+  });
+
+//kiirat  post
+// import express from "express";
+// const app = express();
+//  app.get('/',(req,res)=>{
+//         res.send('MongoDB is connected successfully')
+//     });
+//      app.post('/p',(req,res)=>{
+//         res.send('MongoDB is connected successfully at POST')
+//     })
+//     app.listen(process.env.PORT || 3000, () => {
+//       console.log(`App is listening on http://localhost:${process.env.PORT || 3000}`);
+//     });
 
 //2 do DB related operations on same file
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants.js";
-
-// import express from "express";
-// const app = express();
 
 // import dotenv from "dotenv";
 // dotenv.config();
