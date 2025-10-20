faee715b0a13de2d33455a94411d2a75100cbabc
Avez Chadchan <avezchadchan@gmail.com>
2025-10-21 01:15:27 +0530

    cloudinary setup and multer

---
+++ production_backend/src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"; //file system used to read,write,change permission,append,etc thefile
+
+//unlink = delete
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.v2.uploader
+      .upload(localFilePath, {
+        resource_type: "auto",
+      })
+      .then((res) => console.log(res));
+    console.log("successfully uploaded the file", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //it removes the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+// cloudinary.v2.uploader
+//   .upload("sample.jpg", { width: 2000, height: 1000, crop: "limit" })
+//   .then((result) => console.log(result));
+
+export { uploadOnCloudinary  };
+ 
